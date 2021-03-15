ac80537e95f92881e4c285e5052a34cb73c48c77
Alexander Tih <dgioto@gmail.com>
2021-03-15 10:27:13 +0200

    first update

--- app/src/main/java/com/example/fyz/Thing.java
+++ app/src/main/java/com/example/fyz/Thing.java
@@ -7,16 +7,16 @@ public class Thing {
 
     // drinks is an array of Drinks
     public static final Thing[] drinks = {
-            new Thing("Latte", "A copule of espresso shot with steamed milk",
+            new Thing("Latte", "A couple of espresso shot with steamed milk",
                     R.drawable.latte),
-            new Thing("Cappuccino", "Esprecco, hot milk, and a steamed milk foam",
+            new Thing("Cappuccino", "Espresso, hot milk, and a steamed milk foam",
                     R.drawable.cappuccino),
-            new Thing("Filter", "Hight quality beans roasted and brewed fresh",
+            new Thing("Filter", "Height quality beans roasted and brewed fresh",
                     R.drawable.filter)
     };
 
     //Each Drink has a name, description, and an image resource
-    private Drink(String name, String description, int imageResourceId){
+    private Thing(String name, String description, int imageResourceId){
         this.name = name;
         this.description = description;
         this.imageResourceId = imageResourceId;
