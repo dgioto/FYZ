dd484d816dddda31b7d967726416c548f9c374e4
Alexander Tih <dgioto@gmail.com>
2021-05-05 23:33:43 +0300

    add menu: search and home

--- app/src/main/java/com/dgioto/fyz/MainActivity.java
+++ app/src/main/java/com/dgioto/fyz/MainActivity.java
@@ -1,18 +1,62 @@
 package com.dgioto.fyz;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.MenuItemCompat;
 
 import android.app.SearchManager;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.SearchView;
+import android.widget.ShareActionProvider;
+
+import static androidx.core.view.MenuItemCompat.getActionProvider;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ShareActionProvider shareActionProvider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        MenuItem idSearch = menu.findItem(R.id.id_search);
+        SearchView sv = (SearchView) idSearch.getActionView();
+        sv.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                return false;
+            }
+        });
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.web_search:
+                Intent intent = new Intent(Intent.ACTION_WEB_SEARCH);
+                intent.putExtra(SearchManager.QUERY, "fyz.com.ua");
+                startActivity(intent);
+                return  true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     public void onClickCategories(View view){
@@ -44,12 +88,6 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(
                 MainActivity.this,
                 AboutUs.class);
-        startActivity(intent);
-    }
-
-    public void onClickWeb(View view){
-        Intent intent = new Intent(Intent.ACTION_WEB_SEARCH);
-        intent.putExtra(SearchManager.QUERY, "fyz.com.ua");
         startActivity(intent);
     }
 }
